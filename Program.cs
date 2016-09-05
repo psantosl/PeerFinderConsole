d6cf1ba32ca9a5ca7beddb1fcd40c4c489055b8a
pablo <>
2016-09-06 00:40:55 +0200

    Simplified code and re-tested:
    >PeerFinder.exe host
    Connection requested by backyard. Will be accepted automatically.
    Receiver waiting to connect async
    Receiver connection happened
    Going to read 67108864 bytes
    0/67108864
    33554432/67108864
    Received 64 MB in 6 secs = 10.67 MB/s
    
    
    >PeerFinder.exe client
    Peer found: PeerFinderConsoleApp
    Going to send 67108864 bytes
    Sent 64 MB in 8 secs = 8 MB/s

--- Program.cs
+++ Program.cs
@@ -29,6 +29,7 @@ namespace PeerFinderConsole
             {
                 PeerFinder.Role = PeerRole.Client;
                 PeerFinder.AllowWiFiDirect = true;
+                PeerFinder.TriggeredConnectionStateChanged += TriggeredConnectionStateChanged;
 
                 if ((Windows.Networking.Proximity.PeerFinder.SupportedDiscoveryTypes &
                      Windows.Networking.Proximity.PeerDiscoveryTypes.Browse) !=
@@ -74,32 +75,35 @@ namespace PeerFinderConsole
 
                     Console.WriteLine("Going to send {0} bytes", totalToSend);
 
-                    long sent = 0;
+                    byte[] buffer = new byte[totalToSend];
 
-                    byte[] buffer = new byte[32 * 1024 * 1024];
+                    writer.Write(buffer);
 
-                    while (sent < totalToSend)
-                    {
-                        long toSend = totalToSend - sent;
+                    writer.Flush();
 
-                        if (toSend >= buffer.Length)
-                        {
-                            toSend = buffer.Length;
-                        }
+                    reader.ReadBoolean();
 
-                        writer.Write(buffer, 0, (int)toSend);
+                    float MB = totalToSend / 1024 / 1024;
+                    float secs = (Environment.TickCount - ini) / 1000;
 
-                        Console.WriteLine("{0}/{1}", sent, totalToSend);
+                    Console.WriteLine("Sent {0} MB in {1} secs = {2:#0.##} MB/s",
+                        MB, secs, MB / secs);
+                }
+            }
 
-                        writer.Flush();
+            void TriggeredConnectionStateChanged(
+                object sender,
+                Windows.Networking.Proximity.TriggeredConnectionStateChangedEventArgs e)
+            {
+                Console.WriteLine("Peer found! " + e.Id);
 
-                        reader.ReadBoolean();
-
-                        sent += toSend;
-                    }
-
-                    Console.WriteLine("Written {0} bytes in {1} sec",
-                        totalToSend, (Environment.TickCount - ini) / 1000);
+                if (e.State == Windows.Networking.Proximity.TriggeredConnectState.PeerFound)
+                {
+                    Console.WriteLine("Peer found. You may now pull your devices out of proximity.\n");
+                }
+                if (e.State == Windows.Networking.Proximity.TriggeredConnectState.Completed)
+                {
+                    Console.WriteLine("Connected. You may now send a message.\n");
                 }
             }
         }
@@ -122,8 +126,6 @@ namespace PeerFinderConsole
 
                 PeerFinder.ConnectionRequested += ConnectionRequested;
 
-                PeerFinder.TriggeredConnectionStateChanged += TriggeredConnectionStateChanged;
-
                 PeerFinder.Start();
 
                 Console.ReadLine();
@@ -144,22 +146,6 @@ namespace PeerFinderConsole
                 catch (Exception ex)
                 {
                     Console.WriteLine("Error in receiver: {0}", ex.Message + Environment.NewLine + ex.StackTrace);
-                }
-            }
-
-            void TriggeredConnectionStateChanged(
-                object sender,
-                Windows.Networking.Proximity.TriggeredConnectionStateChangedEventArgs e)
-            {
-                Console.WriteLine("Peer found! " + e.Id);
-
-                if (e.State == Windows.Networking.Proximity.TriggeredConnectState.PeerFound)
-                {
-                    Console.WriteLine("Peer found. You may now pull your devices out of proximity.\n");
-                }
-                if (e.State == Windows.Networking.Proximity.TriggeredConnectState.Completed)
-                {
-                    Console.WriteLine("Connected. You may now send a message.\n");
                 }
             }
 
@@ -207,8 +193,11 @@ namespace PeerFinderConsole
                         writer.Flush();
                     }
 
-                    Console.WriteLine("Read {0} bytes in {1} sec",
-                        totalToRead, (Environment.TickCount - ini) / 1000);
+                    float MB = totalToRead / 1024 / 1024;
+                    float secs = (Environment.TickCount - ini) / 1000;
+
+                    Console.WriteLine("Received {0} MB in {1} secs = {2:#0.##} MB/s",
+                        MB, secs, MB / secs);
                 }
             }
         }
@@ -216,7 +205,6 @@ namespace PeerFinderConsole
         //https://social.msdn.microsoft.com/Forums/en-US/ce649545-9ec6-45da-a4ee-71b9f2bed156/using-metro-win8-sdk-to-build-desktop-style-application?forum=winappswithnativecode
 
         [DllImport("shell32.dll")]
-        public static extern int SetCurrentProcessExplicitAppUserModelID([MarshalAs(UnmanagedType.LPWStr)] string AppID);
-
+        static extern int SetCurrentProcessExplicitAppUserModelID([MarshalAs(UnmanagedType.LPWStr)] string AppID);
     }
 }
